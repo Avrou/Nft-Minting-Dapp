5d39de467f65ded0bc920d6fc41a57d6137d16f3
Mouazzama <mouazzamabinteumer.ep19102044@gmail.com>
2023-01-02 22:51:21 -0800

    first commit

---
+++ next js/pages/api/[tokenId].js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default function handler(req, res) {
+  const tokenId = req.query.tokenId;
+
+  const name =  `Casual Nft Mint # ${tokenId}`;
+  const description = `Casual Nft is collection for web3 developer`
+  const image = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${Number(tokenId-1)}.svg`
+  // const image = `https://gateway.pinata.cloud/ipfs/QmaWAQsgPjDv65h6ahU2i4jtwbVGNtdAK71WbG8XzZ4F8p/`
+  return res.json({
+    name: name,
+    description: description,
+    image: image,
+  });
+}
